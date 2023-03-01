b40601de0a2908c1b743151841240d3fbfcbc1a8
unkerpete <peterkjx@gmail.com>
2023-03-01 23:27:14 +0800

    Installed tailwind css.
    On first install attempt per tailwind documentation, tailwind codes were working but just not on <div> tags. Fixed by adding in the tailwind.config.cjs file

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import { useState } from 'react';
 import './App.css';
+import Navbar from './components/navbar';
 
 function App() {
-  return <div className="App">hello</div>;
+  return (
+    <div>
+      <Navbar />
+      <div className="text-3xl font-bold underline header">hello</div>
+      <div className="inline-block">div2</div>
+      <div>div3</div>
+      <div>div4</div>
+    </div>
+  );
 }
 
 export default App;
