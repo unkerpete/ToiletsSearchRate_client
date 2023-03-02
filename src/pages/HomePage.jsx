f1ae11b946fa1dfafd8016b896cfa50067dfa0d0
unkerpete <peterkjx@gmail.com>
2023-03-02 22:11:46 +0800

    Created HomePage and UserLoginPage.
    Encountered a weird problem. Vite was not HMRing the files, HomePage and UserLoginPage which were inside my 'pages' folder. Solved this problem by moving both files to the 'components' folder and HMR was working again for said files. Then moved them back into the 'pages' folder and HMR remains working.
    
    Weird?

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import FilterAndDisplay from '../components/FilterAndDisplay';
+
+const HomePage = () => {
+  return (
+    <div>
+      {/* someinfo component here */}
+      <FilterAndDisplay />
+      {/* map component here */}
+    </div>
+  );
+};
+
+export default HomePage;
---
+++ src/pages/UserLoginPage.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+const UserLoginPage = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleUsernameChange = (e) => {
+    setUsername(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Username: ${username}, Password: ${password}`);
+  };
+
+  return (
+    <div className="mt-28">
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          id="username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          id="password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default UserLoginPage;
