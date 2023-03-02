c824baf58786e50b929d6ba45d48b23f11de5537
unkerpete <peterkjx@gmail.com>
2023-03-03 00:30:36 +0800

    Navbar wip1. Added /home link. Added some styling

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -4,11 +4,29 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {
   return (
     <>
-      <div>
-        <Link to="/login" className="m-28">
-          Login
-        </Link>
-        <Link to="/register">Register</Link>
+      <div className="flex justify-between">
+        <span className="text-2xl font-bold">{`Welcome, {state here that tracks username or guest}`}</span>
+
+        <div className="flex items-center">
+          <Link
+            to="/home"
+            className="mr-4 px-3 py-1 rounded-lg bg-gray-300 hover:bg-gray-400"
+          >
+            Home
+          </Link>
+          <Link
+            to="/login"
+            className="mr-4 px-3 py-1 rounded-lg bg-gray-300 hover:bg-gray-400"
+          >
+            Login
+          </Link>
+          <Link
+            to="/register"
+            className="px-3 py-1 rounded-lg bg-gray-300 hover:bg-gray-400"
+          >
+            Register
+          </Link>
+        </div>
       </div>
     </>
   );
