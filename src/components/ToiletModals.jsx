9e99bb7e006a9c7142e9da40649518112449228d
unkerpete <peterkjx@gmail.com>
2023-03-08 01:36:13 +0800

    ToiletModals wip5. Added functionality to the likes and dislikes.
    A user can either like(heart) or dislike(poop) per toilet. User cannot like AND dislike a toilet. User can change vote from heart to poop and vice versa by clicking on the other icon.

--- src/components/ToiletModals.jsx
+++ src/components/ToiletModals.jsx
@@ -9,6 +9,8 @@ import closeIcon from '../assets/icons/close.png';
 const ToiletModals = (props) => {
   const { id, sex, bidet, _location, _address, imgurl, details } = props.item;
   const [comments, setComments] = useState();
+  const [likes, setLikes] = useState();
+  const [dislikes, setDislikes] = useState();
 
   const showToastMessage = (type, message) => {
     toast[type](message, {
@@ -28,8 +30,56 @@ const ToiletModals = (props) => {
     }
   };
 
+  const getLikesAPI = async () => {
+    try {
+      const res = await fetch(`http://127.0.0.1:5001/likes/getlikes/${id}`);
+      const json = await res.json();
+      setLikes(json);
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  const getDislikesAPI = async () => {
+    try {
+      const res = await fetch(`http://127.0.0.1:5001/likes/getdislikes/${id}`);
+      const json = await res.json();
+      setDislikes(json);
+    } catch (err) {
+      alert(err.message);
+    }
+  };
+
+  const submitLikeDislike = async (like) => {
+    let data = { toilets_id: id, users_username: props.userName, liked: like };
+    try {
+      const res = await fetch(`http://127.0.0.1:5001/likes/submitlikes`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: localStorage.getItem('token'),
+        },
+        body: JSON.stringify(data),
+      });
+      getLikesAPI();
+      getDislikesAPI();
+      const json = await res.json();
+      if (
+        json ===
+        'null value in column "users_username" of relation "ratings" violates not-null constraint'
+      ) {
+        showToastMessage('error', 'Need to be logged in to submit vote');
+      }
+    } catch (err) {
+      console.log(err.message);
+      showToastMessage('error', 'failed to submit rating ' + err.message);
+    }
+  };
+
   useEffect(() => {
     getCommentsAPI();
+    getLikesAPI();
+    getDislikesAPI();
   }, []);
 
   const handleSubmitNewCommentClick = async () => {
@@ -56,6 +106,7 @@ const ToiletModals = (props) => {
           'You need to be signed in to submit a comment.'
         );
       }
+      document.getElementById('newcomment').value = '';
     } catch (err) {
       showToastMessage('error', 'submit message failed ' + err.message);
       console.log(err);
@@ -64,26 +115,26 @@ const ToiletModals = (props) => {
   };
 
   const handleHeartClicked = () => {
-    console.log('heart clicked');
+    submitLikeDislike(true);
   };
 
   const handlePoopClicked = () => {
-    console.log('poop clicked');
+    submitLikeDislike(false);
   };
 
   return (
     <div className="fixed top-0 left-0 right-0 bottom-0 bg-gray-900 bg-opacity-50 flex justify-center z-10 flex justify-center items-center">
       <ToastContainer />
-      <div className="bg-white rounded-lg p-4 h-5/6 w-1/3 relative">
+      <div className="bg-white rounded-lg p-4 h-5/6 w-2/5 relative">
         <div className="inline-block absolute top-0 right-0 p-4 h-auto w-14 hover:cursor-pointer">
           <img src={closeIcon} alt="" onClick={props.handleCloseModal} />
         </div>
         <span className="absolute top-0 left-4 text-xs">{`Toilet ID: ${id}`}</span>
         <div className="flex">
-          <div className="img-container inline-block w-80 h-auto">
+          <div className="img-container inline-block w-1/2">
             <img src={imgurl} alt="" className="shadow-lg rounded-lg" />
           </div>
-          <div className="ratingsDisplayAndDetails-container w-1/2 inline-block pl-1">
+          <div className="ratingsDisplayAndDetails-container w-1/2 inline-block pl-3">
             <div className="flex">
               <div className="flex">
                 <img
@@ -92,7 +143,7 @@ const ToiletModals = (props) => {
                   className="inline-block h-auto w-5 hover:cursor-pointer"
                   onClick={handleHeartClicked}
                 />
-                <span className="ml-1">5</span>
+                <span className="ml-1">{likes}</span>
               </div>
               <div className="flex ml-4">
                 <img
@@ -101,10 +152,10 @@ const ToiletModals = (props) => {
                   className="inline-block h-auto w-5 hover:cursor-pointer"
                   onClick={handlePoopClicked}
                 />
-                <span className="ml-1">5</span>
+                <span className="ml-1">{dislikes}</span>
               </div>
             </div>
-            <div className="details-container mt-2 text-left text-xs">
+            <div className="details-container mt-2 text-left text-sm">
               <p className="block">
                 <span className="font-bold underline">Location</span>
                 <br />
@@ -141,7 +192,7 @@ const ToiletModals = (props) => {
             type="text"
             id="newcomment"
             placeholder="Write your review here"
-            className="w-full rounded-lg px-3 py-2 mb-1 resize-y border h-10 text-sm"
+            className="w-full rounded-lg px-2 py-2 mb-1 resize-none h-10 border text-sm"
           ></textarea>
         </div>
         <div className="submitCommentBtn-container text-right text-xs">
